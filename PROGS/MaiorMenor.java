a5f0a169a32b5a7648643be4e698e05272b1b230
HEITOR PONTIERI DE OLIVEIRA <22102363@CFP127ALUNO.LOCAL>
2022-03-09 10:27:13 -0300

    estrutura de decisão

---
+++ PROGS/MaiorMenor.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+import javax.swing.JOptionPane;
+
+
+public class MaiorMenor {
+
+    public static void main(String[] args) {
+         JOptionPane sim = new JOptionPane();
+
+         String idadeString = sim.showInputDialog(null, "Quantos anos vc tem lek?");
+
+         int idadeInt = Integer.parseInt(idadeString);
+
+         if (idadeInt > 13) {
+                sim.showMessageDialog(null, "Ih ala, para pf, vc nao tem mais idade pra ficar jogando esse fortnite mano");
+
+
+         }
+         else {
+            sim.showMessageDialog(null, " Dessa vez vc passou, mas ja vai tomando jeito e vai saindo desse mundo de fortnite ein");
+         }
+
+    }
+
+    
+    
+}
