fdf057ff931a36d3db89b0cb28b9acb68e2c8696
HEITOR PONTIERI DE OLIVEIRA <22102363@CFP127ALUNO.LOCAL>
2022-03-28 10:25:28 -0300

    q

--- PROGS/Fatorial.java
+++ PROGS/Fatorial.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import javax.print.event.PrintJobListener;
+
 public class Fatorial {
     public static void main(String[] args) {
 
@@ -10,9 +12,10 @@ public class Fatorial {
         int n = ler.nextInt();
         int Fat = 1;
         
-
+        System.out.println();
         for(int i = 1; i<=n; i++){ 
             System.out.println (Fat = Fat * i);
+            
         }
     }
 }
