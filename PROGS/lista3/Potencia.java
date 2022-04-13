f6b338e1aa002bf2f77258f6516f0b5f6b1729da
HEITOR PONTIERI DE OLIVEIRA <22102363@CFP127ALUNO.LOCAL>
2022-04-13 08:14:52 -0300

    f

--- PROGS/lista3/Potencia.java
+++ PROGS/lista3/Potencia.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
 
 public class Potencia {
     public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int base = 0;
+        int expoente =0;
+
+        while(true){
+            System.out.println("\nDigite o valor de uma base maior que 0");
+            base =ler.nextInt();
+            System.out.println("\nDigite o valor do expoente maior que 0");
+            expoente = ler.nextInt();
+            if (base  >=0 && expoente >=0 ) {
+                break; 
+            }
+        }
+
+        long pot = base;
+
+        if (expoente == 0) {
+            System.out.println(base + "^" + expoente + "= 1");
+            System.exit(0);            
+        }
+
+        for(int i=1 ; i<expoente ; i++){
+            //pot = pot * base
+            pot*=base;
+        }
+        
+        System.out.println(base +"^ " + expoente + "="+ pot);
         
     }
+
     
 }
